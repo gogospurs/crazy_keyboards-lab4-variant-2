26ba348da2eb7ebda2e8973fb2f50eebdeeb1511
Yu Zhang <dante.yu@hdu.edu.cn>
2022-06-20 22:48:45 +0800

    initial

--- example_func_test.py
+++ example_func_test.py
@@ -27,19 +27,19 @@ class Testmultiple(unittest.TestCase):
         # two floats
         if (not (math.isinf(a) or math.isinf(b)) and
                 (not (math.isnan(a) or math.isnan(b)))):
-            self.assertEqual(1e5*(add(a, b)-a-b) < 1, True)
-            self.assertEqual(1e5*(add(a, b)-add(b, a)) < 1, True)
+            self.assertEqual(add(a, b), a+b)
+            self.assertEqual(add(a, b), add(b, a))
 
         # optional
-            self.assertEqual(1e5*(add(a)-a) < 1, True)
+            self.assertEqual(add(a), a)
 
     @given(st.integers(), st.floats())
     def test_add_IntAndFloat(self, a, b):
         '''test int and float'''
         # two numbers
         if (not math.isinf(b)) and (not math.isnan(b)):
-            self.assertEqual(1e5(add(a, b)-(a+b)) < 1, True)
-            self.assertEqual(1e5(add(a, b)-add(b, a)) < 1, True)
+            self.assertEqual(add(a, b), a+b)
+            self.assertEqual(add(a, b), add(b, a))
 
     @given(st.text(), st.text())
     def test_add_str(self, a, b):
